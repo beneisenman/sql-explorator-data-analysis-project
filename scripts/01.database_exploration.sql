be38385a57abf694999db4ed47e59a1080843373
beneisenman <beneisenman@gmail.com>
2026-02-08 18:06:50 -0800

    Create 01.database_exploration.sql

---
+++ scripts/01.database_exploration.sql
@@ -0,0 +1,23 @@
+/*
+===============================================================================
+Database Exploration
+===============================================================================
+Purpose:
+    - To explore the structure of the database, including the list of tables and their schemas.
+    - To inspect the columns and metadata for specific tables.
+
+Table Used:
+    - INFORMATION_SCHEMA.TABLES
+    - INFORMATION_SCHEMA.COLUMNS
+===============================================================================
+*/=
+
+SELECT 
+	* 
+FROM INFORMATION_SCHEMA.TABLES
+
+--Explore All Columns in the Database
+SELECT 
+	* 
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'dim_customers'
