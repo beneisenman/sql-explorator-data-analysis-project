6795f009661350b834024e7763a63d8fa4ff775b
beneisenman <beneisenman@gmail.com>
2026-02-08 18:15:54 -0800

    Create 08_cumulative_analysis.sql

---
+++ scripts/08_cumulative_analysis.sql
@@ -0,0 +1,22 @@
+/*
+===============================================================================
+Cumulative Analysis
+===============================================================================
+Purpose:
+    - To calculate running totals or moving averages for key metrics.
+    - To track performance over time cumulatively.
+    - Useful for growth analysis or identifying long-term trends.
+
+SQL Functions Used:
+    - Window Functions: SUM() OVER(), AVG() OVER()
+===============================================================================
+*/
+
+-- Calculate the total sales per month 
+-- and the running total of sales over time 
+SELECT
+	order_date,
+	sales_amount,
+	SUM(sales_amount) OVER (PARTITION BY order_Date ORDER BY order_date) AS running_total
+FROM gold.fact_sales 
+WHERE order_date IS NOT NULL
